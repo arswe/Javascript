3df228c9747e048551fd1cca661bcea030ebf6ce
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-28 17:55:10 +0600

    if else

---
+++ Part-1/Control Flow/ IfElse.js
@@ -0,0 +1,6 @@
+let hour = 10;
+if (hour >= 6 && hour < 12) {
+  console.log('Good Morning');
+} else if (hour >= 12 && hour < 12) {
+  console.log('Good Afternoon');
+} else console.log('Good Evening');
