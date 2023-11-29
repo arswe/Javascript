4ea6894ff17cb12ad0ebb2d84d19ca935eedb3b1
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-29 18:43:39 +0600

    calculate Average

--- Part-1/Control Flow/Grade.js
+++ Part-1/Control Flow/Grade.js
@@ -2,12 +2,16 @@ const marks = [80, 80, 50];
 console.log(calculateGrade(marks));
 
 function calculateGrade(marks) {
-  let sum = 0;
-  for (let mark of marks) sum += mark;
-  let average = sum / marks.length;
+  const average = calculateAverage(marks);
   if (average < 60) return 'F';
   if (average < 70) return 'D';
   if (average < 80) return 'C';
   if (average < 90) return 'B';
   return 'A';
+}
+
+function calculateAverage(array) {
+  let sum = 0;
+  for (let value of array) sum += value;
+  return sum / array.length;
 }
