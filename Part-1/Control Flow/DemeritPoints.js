5135043d1c7c088fe9b7fc65604b16cf0d332ccd
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-29 18:21:08 +0600

    checkSpeed

--- Part-1/Control Flow/DemeritPoints.js
+++ Part-1/Control Flow/DemeritPoints.js
@@ -9,7 +9,7 @@ function checkSpeed(speed) {
     return;
   }
 
-  const points = Math.floor((speed - speedLimit) / kmPerPoint);
-  if (points >= 12) console.log('License suspended');
-  else console.log('Points', points);
+  const points = Math.floor((speed - speedLimit) / kmPerPoint); // 12.4 => 12
+  if (points >= 12) console.log('License suspended'); // 12 points => License suspended
+  else console.log('Points', points); // 1-11 points => Points 1-11
 }
