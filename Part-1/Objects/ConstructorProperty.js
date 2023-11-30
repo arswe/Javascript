19e4c2bed251c2b1434ba773d570d546afab5729
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-30 17:35:22 +0600

    Constructor Property.mp4

---
+++ Part-1/Objects/ConstructorProperty.js
@@ -0,0 +1,36 @@
+// Factory Function
+
+// function createCircle(radius) {
+//   return {
+//     radius,
+//     drow() {
+//       console.log('draw');
+//     },
+//   };
+// }
+
+// const circle = createCircle(1);
+// circle.drow();
+// console.log(circle.radius);
+
+// Constructor Function
+
+// function Circle(radius) {
+//   this.radius = radius;
+//   this.drow = function () {
+//     console.log('draw');
+//   };
+// }
+
+// const circle = new Circle(1);
+// console.log(circle.constructor);
+
+// circle.drow();
+// console.log(circle.radius);
+
+let x = {};
+new String(); // '', "", ``
+new Boolean(); // true, false
+new Number(); // 1, 2, 3, ...
+
+// let x = new Object();
