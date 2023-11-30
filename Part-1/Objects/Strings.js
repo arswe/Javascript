365a8818a1bc5787f7bd26782b7bcd4d3df239aa
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-30 18:16:06 +0600

    String

---
+++ Part-1/Objects/Strings.js
@@ -0,0 +1,14 @@
+// String Primitive
+const message = ' this is my first message ';
+console.log(message.includes('my'));
+console.log(message.includes('not'));
+console.log(message.endsWith('this'));
+console.log(message.indexOf('my'));
+console.log(message.toUpperCase());
+console.log(message.toLowerCase());
+console.log(message.trim());
+console.log(message);
+
+// String Objects
+const another = new String('hi');
+console.log(another);
