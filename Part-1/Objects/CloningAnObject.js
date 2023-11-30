507dd353bfd8e596e4919d55517fdd5adc04e954
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-30 18:05:04 +0600

    Cloning an Object

---
+++ Part-1/Objects/CloningAnObject.js
@@ -0,0 +1,17 @@
+const circle = {
+  radius: 1,
+  drow() {
+    console.log('Drow');
+  },
+};
+
+// const another = {};
+
+// for (const key in circle) {
+//   another[key] = circle[key];
+// }
+
+// const another = Object.assign({}, circle);
+// const another = { ...circle };
+
+console.log(another);
