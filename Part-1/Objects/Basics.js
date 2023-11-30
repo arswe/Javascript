8d66aba29385f866cfce20a38f75a122a52d7060
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-30 17:12:08 +0600

    start with object basics

---
+++ Part-1/Objects/Basics.js
@@ -0,0 +1,20 @@
+let radius = 1;
+let x = 1;
+let y = 1;
+
+const circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  isVasible: true,
+};
+
+function drow() {
+  console.log('draw');
+}
+
+function move() {
+  console.log('move');
+}
