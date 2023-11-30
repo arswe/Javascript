f0ef897937970eb2a967b5cd6653afe921599429
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-30 23:03:26 +0600

    Exercise 4- Blog Post Object

---
+++ Part-1/Objects/Exercise4.js
@@ -0,0 +1,13 @@
+const blogPost = {
+  title: 'a',
+  body: 'b',
+  author: 'c',
+  views: 10,
+  comments: [
+    { author: 'a', body: 'b' },
+    { author: 'c', body: 'd' },
+  ],
+  isLive: true,
+};
+
+console.log(blogPost);
