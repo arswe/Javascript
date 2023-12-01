6bc63c7882279c50629f79173699a91fbaf54f66
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-01 10:31:47 +0600

    Finind Elements

--- Part-1/Arrays/FindingElements.js
+++ Part-1/Arrays/FindingElements.js
@@ -3,3 +3,7 @@ const numbers = [1, 2, 3, 4, 1, 1];
 console.log(numbers.indexOf('a')); // -1
 console.log(numbers.indexOf(1)); // 0
 console.log(numbers.indexOf(1, 2)); // 4
+console.log(numbers.lastIndexOf(1)); // 5
+console.log(numbers.indexOf(1) !== -1); // true
+console.log(numbers.includes(1)); // true
+console.log(numbers.indexOf(1, 2) !== -1); // true
