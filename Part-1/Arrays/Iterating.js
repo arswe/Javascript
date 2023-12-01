61526f73c00a989e51e712ad87e7baaf4cb6546f
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-01 17:45:47 +0600

    iteration

--- Part-1/Arrays/Iterating.js
+++ Part-1/Arrays/Iterating.js
@@ -1,10 +1,10 @@
 const numbers = [1, 2, 3, 4, 5, 6];
-
-for (let number of numbers) console.log(number);
-numbers.forEach(function (number) {
-  console.log(number);
-});
+// array iteration
+// for (let number of numbers) console.log(number);
+// numbers.forEach(function (number) {
+//   console.log(number);
+// });
 
 numbers.forEach((number) => console.log(number));
 numbers.forEach((number, index) => console.log(index, number));
-numbers.forEach((number, index) => console.log(index, number, numbers));
+// numbers.forEach((number, index) => console.log(index, number, numbers));
