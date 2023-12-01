bbb895a3f40af036d9ca8ccd92aed5a70076fffb
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-01 17:35:38 +0600

    Emptying an Array

---
+++ Part-1/Arrays/EmptyingAnArray.js
@@ -0,0 +1,21 @@
+let numbers = [1, 2, 3, 4, 5, 6];
+let another = numbers;
+
+// numbers = [];
+
+// console.log(numbers);
+// console.log(another);
+
+// numbers.length = 0;
+// console.log(numbers);
+// console.log(another);
+
+// numbers.splice(0, numbers.length);
+// console.log(numbers);
+// console.log(another);
+
+while (numbers.length > 0) {
+  numbers.pop();
+}
+console.log(numbers);
+console.log(another);
