8e8d84debc9ab33e1cd256d698b8d07f8225fd81
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-01 17:42:58 +0600

    The Spread Operator

--- Part-1/Arrays/SpreadOperator.js
+++ Part-1/Arrays/SpreadOperator.js
@@ -1,5 +1,7 @@
 const first = [1, 2, 3];
 const second = [4, 5, 6];
 
-const combined = [...first, ...second]; // The spread operator is used to spread the elements of an array.
+const combined = [...first, 'a', ...second, 'b']; // The spread operator is used to spread the elements of an array.
+const copy = [...combined]; // The spread operator is also used to copy an array.
 console.log(combined);
+console.log(copy);
