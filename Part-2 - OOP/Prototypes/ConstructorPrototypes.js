1fe68c12d329c8f69944659727d0e63ebcba3132
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-03 19:39:47 +0600

    Constructor Prototypes

---
+++ Part-2 - OOP/Prototypes/ConstructorPrototypes.js
@@ -0,0 +1,11 @@
+// Constructor Prototypes
+Object.getPrototypeOf(myObj);
+
+// myObj.__proto__;
+
+function Circle(radius) {
+  this.radius = radius;
+}
+
+Circle.prototype;
+const circle = new Circle(1);
