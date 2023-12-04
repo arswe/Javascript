1abee3a2429d2461e7876f61a345cef825fb9ed7
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-04 08:45:30 +0600

    Resetting the Constructor

---
+++ Part-2 - OOP/Prototypical Inheritance/ResettingTheConstructor.js
@@ -0,0 +1,22 @@
+//  Resetting the Constructor example:
+
+function Shape() {}
+
+Shape.prototype.duplicate = function () {
+  console.log('duplicate');
+};
+
+function Circle(radius) {
+  this.radius = radius;
+}
+// Circle.prototype.constructor = Circle; // This is the default value
+// new Circle.prototype.constructor() => new Shape; // This is the default value
+
+Circle.prototype = Object.create(Shape.prototype); // Circle.prototype.__proto__ = Shape.prototype
+Circle.prototype.constructor = Circle; // This is the default value
+
+Circle.prototype.draw = function () {
+  console.log('draw');
+};
+
+const s = new Shape();
