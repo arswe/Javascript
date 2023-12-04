30fb10907447cecf43e5ae32dc3feede5ce12a97
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-04 08:39:09 +0600

    Creating Your Own Prototypical Inheritance

---
+++ Part-2 - OOP/Prototypical Inheritance/OwnPrototypicalInheritance.js
@@ -0,0 +1,20 @@
+// Own Prototypical Inheritance example
+
+function Shape() {}
+
+Shape.prototype.duplicate = function () {
+  console.log('duplicate');
+};
+
+function Circle(radius) {
+  this.radius = radius;
+}
+
+Circle.prototype = Object.create(Shape.prototype); // Circle.prototype.__proto__ = Shape.prototype
+
+Circle.prototype.draw = function () {
+  console.log('draw');
+};
+
+const s = new Shape();
+const c = new Circle(1);
