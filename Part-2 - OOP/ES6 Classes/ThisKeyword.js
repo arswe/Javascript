fae58ebc67d59a603a926acdabe6e9b9b9c2a2a2
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-04 17:43:33 +0600

    The This Keyword

---
+++ Part-2 - OOP/ES6 Classes/ThisKeyword.js
@@ -0,0 +1,17 @@
+'use strict';
+//  The This Keyword
+
+const Circle = function () {
+  this.draw = function () {
+    console.log(this);
+  };
+};
+
+const c = new Circle();
+// Method Call
+c.draw();
+
+const draw = c.draw;
+
+// Function Call
+draw();
