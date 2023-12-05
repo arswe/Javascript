7631d6591d37864666d0344cd90f76245afcbfef
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-05 23:43:00 +0600

    linearSearch() function implementation

---
+++ Part-3/Exercise2.js
@@ -0,0 +1,18 @@
+// input: linearSearch(['a', 'b', 'c', 'd', 'e'], 'a')
+// output: 0
+
+// problem: linearSearch() function implementation
+
+function linearSearch(array, value) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === value) return i;
+  }
+  return -1;
+}
+
+console.log(linearSearch(['a', 'b', 'c', 'd', 'e'], 'a'));
+console.log(linearSearch(['a', 'b', 'c', 'd', 'e'], 'z'));
+console.log(linearSearch(['a', 'b', 'c', 'd', 'e'], 'e'));
+console.log(linearSearch(['a', 'b', 'c', 'd', 'e'], 'b'));
+console.log(linearSearch(['a', 'b', 'c', 'd', 'e'], 'c'));
+console.log(linearSearch(['a', 'b', 'c', 'd', 'e'], 'd'));
