db3894598e4d403dc8c0f3e2d7cbc6a1386eab24
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-05 23:35:51 +0600

    Find the number of words in the sentence.

--- Part-3/Exercise1.js
+++ Part-3/Exercise1.js
@@ -1,1 +1,8 @@
-// 
+//
+
+const sentence =
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, obcaecati? Voluptas similique adipisci amet commodi impedit voluptates iste consequuntur blanditiis nemo eos omnis, dicta eveniet nesciunt perferendis dolores et accusamus ipsam? Voluptate, reiciendis porro molestiae nostrum error maiores nesciunt alias. Soluta corrupti debitis ex at sit, molestiae similique sapiente adipisci. Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus nostrum maiores autem commodi magnam accusamus porro quisquam qui illo repellat?';
+
+// 1. Find the number of words in the sentence.
+const match = sentence.match(/(ipsum)/g);
+console.log(match);
