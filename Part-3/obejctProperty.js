65a72281f19efad38f66cad008d7f2515008f76e
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-06 23:03:19 +0600

    Object property

---
+++ Part-3/obejctProperty.js
@@ -0,0 +1,15 @@
+// Object Property example:
+// 1. Object property shorthand
+
+const name = 'Andrew';
+const userAge = 27;
+
+const user = {
+  name,
+  age: userAge,
+  location: 'Philadelphia',
+};
+
+console.log(user);
+
+
