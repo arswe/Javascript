56e8deb210902a0296d60e2b3eac384adeea3225
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-06 23:06:02 +0600

    Object destructuring

--- Part-3/obejctProperty.js
+++ Part-3/obejctProperty.js
@@ -1,15 +1,29 @@
 // Object Property example:
 // 1. Object property shorthand
 
-const name = 'Andrew';
-const userAge = 27;
+// const name = 'Andrew';
+// const userAge = 27;
 
-const user = {
-  name,
-  age: userAge,
-  location: 'Philadelphia',
+// const user = {
+//   name,
+//   age: userAge,
+//   location: 'Philadelphia',
+// };
+
+// console.log(user);
+
+// 2. Object destructuring
+const product = {
+  label: 'Red notebook',
+  price: 3,
+  stocks: 201,
+  salePrice: undefined,
 };
 
-console.log(user);
+const label = product.label;
+const prices = product.price;
 
-
+const { label: productLabel, price, stocks, salePrice, rating = 5 } = product;
+console.log(productLabel);
+console.log(price);
+console.log(stocks);
