187f13ab6d06213e485fcc0408c90587ee1e3f2e
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-07 15:23:06 +0600

    Sets

---
+++ Part-3/Sets.js
@@ -0,0 +1,64 @@
+// Initialize a set
+// const letters = new Set(['a', 'b', 'c']);
+
+// Create a Set
+// const letters = new Set();
+
+// // Add Values to the Set
+// letters.add('a');
+// letters.add('b');
+// letters.add('c');
+// letters.add('c');
+// letters.add('b');
+// console.log(letters);
+
+// Create Variables
+// const a = 'a';
+// const b = 'b';
+// const c = 'c';
+
+// // Create a Set
+// const letters = new Set();
+
+// // Add Variables to the Set
+// letters.add(a);
+// letters.add(b);
+// letters.add(c);
+
+// // console.log(letters);
+// letters.add('a');
+// letters.add('b');
+// letters.add('c');
+// letters.add('c');
+// letters.add('c');
+// letters.add('c');
+// letters.add('c');
+// letters.add('c');
+
+// console.log(letters);
+
+// Create a Set
+// const letters = new Set(['a', 'b', 'c']);
+
+// // List all entries
+// let text = '';
+// letters.forEach(function (value) {
+//   text += value;
+// });
+// console.log(text);
+
+// console.log(letters.values());
+// console.log(letters.keys());
+
+// Create an Iterator
+// const myIterator = letters.entries();
+
+// // List all Entries
+// let text = '';
+// for (const entry of myIterator) {
+//   text += entry;
+// }
+
+// console.log(text);
+
+// console.log(typeof letters);
