c58e797bfdad03220527cebf55d9f512de980564
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-07 14:45:01 +0600

    Object Constructors

--- Part-3/ObjectConstructors.js
+++ Part-3/ObjectConstructors.js
@@ -1,12 +1,19 @@
+// Objective: Object Constructors
 function Person(first, last, age, eye) {
   this.firstName = first;
   this.lastName = last;
   this.age = age;
   this.eyeColor = eye;
+  this.fullName = function () {
+    return this.firstName + ' ' + this.lastName;
+  };
 }
 
 const myFather = new Person('John', 'Doe', 50, 'blue');
-const myMother = new Person('Sally', 'Rally', 48, 'green');
+console.log(myFather);
 
-document.getElementById('demo').innerHTML =
-  'My father' + myFather.lastName + ' ' + myFather.lastName + ' ' + myFather.age + '. My mother is ' + myMother.age;
+// const myFather = new Person('John', 'Doe', 50, 'blue');
+// const myMother = new Person('Sally', 'Rally', 48, 'green');
+
+// document.getElementById('demo').innerHTML =
+//   'My father' + myFather.lastName + ' ' + myFather.lastName + ' ' + myFather.age + '. My mother is ' + myMother.age;
