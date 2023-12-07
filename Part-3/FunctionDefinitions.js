34938e0baf4423e3393068a1b206a6128861f0e6
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-07 16:09:54 +0600

    Function Hoisting

--- Part-3/FunctionDefinitions.js
+++ Part-3/FunctionDefinitions.js
@@ -1,4 +1,18 @@
 // function Declarations
-function functionName(parameters) {
-  // code to be executed
+// function functionName(parameters) {
+// code to be executed
+// }
+// Function is called, return value will end up in x
+// const x = myFunction(4, 3);
+
+//
+// const myFunction = new Function('a', 'b', 'return a * b');
+// let x = myFunction(4, 3);
+// console.log(x);
+
+// Function Hoisting
+myFunction(5);
+
+function myFunction(y) {
+  console.log(y * y);
 }
