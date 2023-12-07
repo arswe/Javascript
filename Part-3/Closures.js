97e580e20744f2a4413b1829b3c6b551b06bd2df
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-07 22:48:25 +0600

    A Counter Dilemma

--- Part-3/Closures.js
+++ Part-3/Closures.js
@@ -4,7 +4,20 @@
 // }
 // myFunction();
 
-let a = 4; // global
-function myFunction() {
-  return a * a;
+// let a = 4; // global
+// function myFunction() {
+//   return a * a;
+// }
+
+// Initiate counter
+let counter = 0;
+
+// Function to increment counter
+function add() {
+  counter += 1;
 }
+
+// Call add() 3 times
+add();
+add();
+add();
