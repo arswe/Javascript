57492b0b4f67b40ed6978c2ab02de286bcc6926e
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-07 14:37:36 +0600

    Define setters and getters

---
+++ Part-3/ObjAccessor.js
@@ -0,0 +1,60 @@
+// Create an object:
+// const person = {
+//   firstName: 'Abdur',
+//   lastName: 'Rahman',
+//   language: 'En',
+//   set lang(lang) {
+//     this.language = lang;
+//   },
+// };
+
+// Display data from the object using a getter:
+// console.log((person.lang = 'bd'));
+
+// const person = {
+//   firstName: 'John',
+//   lastName: 'Doe',
+//   get fullName() {
+//     return this.firstName + ' ' + this.lastName;
+//   },
+// };
+
+// Display data from the object using a method:
+// console.log(person.fullName);
+
+// Define object
+const obj = { counter: 0 };
+
+// Define setters and getters
+Object.defineProperty(obj, 'reset', {
+  get: function () {
+    this.counter = 0;
+  },
+});
+Object.defineProperty(obj, 'increment', {
+  get: function () {
+    this.counter++;
+  },
+});
+Object.defineProperty(obj, 'decrement', {
+  get: function () {
+    this.counter--;
+  },
+});
+Object.defineProperty(obj, 'add', {
+  set: function (value) {
+    this.counter += value;
+  },
+});
+Object.defineProperty(obj, 'subtract', {
+  set: function (value) {
+    this.counter -= value;
+  },
+});
+
+// Play with the counter:
+obj.reset;
+obj.add = 5;
+obj.subtract = 1;
+obj.increment;
+obj.decrement;
