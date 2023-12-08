22f310101595e96644b0742fe79851fefef84f7a
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-08 16:17:00 +0600

    Callback Pattern

---
+++ Part-3/CallbackPattern.js
@@ -0,0 +1,25 @@
+const paymentSuccess = true;
+const marks = 70;
+
+function enroll(callback) {
+  console.log('Enrolled is in progress...');
+  setTimeout(function () {
+    if (paymentSuccess) callback();
+    else console.log('Enrolled failed');
+  }, 3000);
+}
+
+function progress(callback) {
+  console.log('counrse on progress...');
+  setTimeout(function () {
+    if (marks >= 80) callback();
+    else console.log('You are failed');
+  }, 2000);
+}
+
+function certificate() {
+  console.log('Certificate is generated');
+  setTimeout(function () {
+    console.log('Certificate is sent to your email');
+  }, 1000);
+}
