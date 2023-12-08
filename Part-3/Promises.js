9b88c6766309b4b010c501a02c16946fc336a999
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-08 18:50:22 +0600

    Promise

--- Part-3/Promises.js
+++ Part-3/Promises.js
@@ -1,25 +1,122 @@
 // Promise
 
-const status = true;
+// let myPromise = new Promise(function (myResolve, myReject) {
+// "Producing Code" (May take some time)
 
-console.log('Task 1');
+//   myResolve(); // when successful
+//   myReject(); // when error
+// });
 
-const promise = new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    if (status) {
-      resolve('Promise is resolved! thats means task 2');
-    } else {
-      reject('Promise is rejected!');
-    }
-  }, 3000);
-});
+// "Consuming Code" (Must wait for a fulfilled Promise)
+// myPromise.then(
+//   function (value) {
+//     /* code if successful */
+//   },
+//   function (error) {
+//     /* code if some error */
+//   }
+// );
 
-promise
+// const status = true;
+
+// console.log('Task 1');
+
+// const promise = new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     if (status) {
+//       resolve('Promise is resolved! thats means task 2');
+//     } else {
+//       reject('Promise is rejected!');
+//     }
+//   }, 3000);
+// });
+
+// promise
+//   .then(function (value) {
+//     console.log(value);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+// console.log('Task 3');
+
+// function myDisplayer(some) {
+//   document.getElementById('demo').innerHTML = some;
+// }
+
+// let myPromise = new Promise(function (myResolve, myReject) {
+//   let x = 0;
+
+//   // The producing code (this may take some time)
+
+//   if (x == 0) {
+//     myResolve('OK');
+//   } else {
+//     myReject('Error');
+//   }
+// });
+
+// myPromise.then(
+//   function (value) {
+//     myDisplayer(value);
+//   },
+//   function (error) {
+//     myDisplayer(error);
+//   }
+// );
+
+//  Callback Pattern
+const paymentSuccess = true;
+const marks = 80;
+
+function enroll(callback) {
+  console.log('Enrolled is in progress...');
+
+  const promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      if (paymentSuccess) {
+        resolve();
+      } else {
+        reject('Enrolled is failed');
+      }
+    }, 3000);
+  });
+  return promise;
+}
+
+function progress(callback) {
+  console.log('counrse on progress...');
+
+  const promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      if (marks > 70) {
+        resolve();
+      } else {
+        reject('Course is failed');
+      }
+    }, 3000);
+  });
+
+  return promise;
+}
+
+function certificate() {
+  console.log('Certificate is generated');
+  promise = new Promise(function (resolve, reject) {
+    setTimeout(function (resolve) {
+      resolve('Certificate is generated');
+    }, 3000);
+  });
+  return promise;
+}
+
+enroll()
+  .then(progress)
+  .then(certificate)
   .then(function (value) {
     console.log(value);
   })
   .catch(function (error) {
     console.log(error);
   });
-
-console.log('Task 3');
