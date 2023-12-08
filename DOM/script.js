73d0c76e57721b2289f630bd212d48be6c42aca0
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-08 19:56:33 +0600

    Animation

--- DOM/script.js
+++ DOM/script.js
@@ -1,3 +1,19 @@
-document.getElementById('demo').innerHTML = 'Hello World';
+// document.getElementById('demo').innerHTML = 'Hello World';
 
-console.dir(document.getElementById('demo'));
+// console.dir(document.getElementById('demo'));
+
+// document.getElementsByTagName('p')[0].innerHTML = 'Hello World';
+// document.getElementsByTagName('p')[1].innerHTML = 'Hello World';
+// document.getElementsByClassName('anchor').innerHTML = 'hoc vien lap trinh';
+
+// console.log(document.querySelector('.anchor'));
+
+// document.write(Date());
+
+function validateForm() {
+  const form = document.forms['myForm'];
+  const value = form[fname].value;
+
+  if (value === '') alert('Please input your name');
+  return false;
+}
