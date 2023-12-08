ceadf8aead451fea5e1a3d09b7dff49038fb20f6
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-08 18:24:34 +0600

    Callback Pattern

--- Part-3/CallbackPattern.js
+++ Part-3/CallbackPattern.js
@@ -1,8 +1,10 @@
+// Code: Callback Pattern
 const paymentSuccess = true;
-const marks = 70;
+const marks = 80;
 
 function enroll(callback) {
   console.log('Enrolled is in progress...');
+
   setTimeout(function () {
     if (paymentSuccess) callback();
     else console.log('Enrolled failed');
@@ -11,6 +13,7 @@ function enroll(callback) {
 
 function progress(callback) {
   console.log('counrse on progress...');
+
   setTimeout(function () {
     if (marks >= 80) callback();
     else console.log('You are failed');
@@ -19,7 +22,12 @@ function progress(callback) {
 
 function certificate() {
   console.log('Certificate is generated');
+
   setTimeout(function () {
     console.log('Certificate is sent to your email');
   }, 1000);
 }
+
+enroll(function () {
+  progress(certificate);
+});
