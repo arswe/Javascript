9d7f178083b9369b503d81041d00d795618710ea
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-08 20:48:42 +0600

    Elements (Nodes)

--- DOM/Nodes.js
+++ DOM/Nodes.js
@@ -1,1 +1,16 @@
 // Nodes
+// const para = document.createElement('h1');
+// const node = document.createTextNode('This is new.');
+// para.appendChild(node);
+
+// const element = document.getElementById('div1');
+// const p3 = document.getElementById('p3');
+// element.insertBefore(para, p2);
+
+const para = document.createElement('h1');
+const node = document.createTextNode('This is new.');
+para.appendChild(node);
+
+const element = document.getElementById('div1');
+const child = document.getElementById('p1');
+element.insertBefore(para, child);
