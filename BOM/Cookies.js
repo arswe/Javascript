404fc90b3d82c35bccc67a4dc4fe7c3378494301
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-09 23:48:11 +0600

    getCookie

--- BOM/Cookies.js
+++ BOM/Cookies.js
@@ -1,10 +1,21 @@
 function setCookie(cname, cvalue, exdays) {
-  var d = new Date();
-  d.setTime(d.getTime() + exdays * (24 * 60 * 60 * 1000));
-  var expires = 'expires=' + d.toUTCString();
+  const d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  let expires = 'expires=' + d.toUTCString();
   document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
 }
 
 function getCookie(cname) {
-  console.log(document.cookie);
+  let ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    console.log(c);
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+      console.log(c);
+    }
+    if (c.indexOf(cname) === 0) {
+      return c.substring(cname.length, c.length);
+    }
+  }
 }
