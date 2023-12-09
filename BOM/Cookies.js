e0b990c9909d3d63c456d60cb15c49b0308e9642
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-09 23:54:02 +0600

    checkCookie

--- BOM/Cookies.js
+++ BOM/Cookies.js
@@ -18,4 +18,17 @@ function getCookie(cname) {
       return c.substring(cname.length, c.length);
     }
   }
+  return '';
+}
+
+function checkCookie() {
+  let user = getCookie('username');
+  if (user != '') {
+    alert('Welcome again ' + user);
+  } else {
+    user = prompt('Please enter your name:', '');
+    if (user != '' && user != null) {
+      setCookie('username', user, 365);
+    }
+  }
 }
