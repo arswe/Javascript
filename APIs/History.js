0c0b77e43b63fb1ee2b6908e92b263c4de2ded84
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-10 09:55:06 +0600

    History API

---
+++ APIs/History.js
@@ -0,0 +1,15 @@
+function back() {
+  window.history.back();
+}
+
+function backTo(n) {
+  window.history.go(n);
+}
+
+function forward() {
+  window.history.forward();
+}
+
+function go() {
+  alert(window.history.length);
+}
