e39512f86e34af911e7f88a4113c644b29821f90
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-10 20:11:54 +0600

    JSON Syntax Rules

---
+++ Json/script.js
@@ -0,0 +1,13 @@
+const person = {
+  name: 'John',
+  age: 30,
+  married: true,
+  skills: ['JavaScript', 'TypeScript', 'Java', 'Python'],
+  book: {
+    name: 'Harry Potter',
+  },
+  alive: true,
+  disease: null,
+};
+
+console.log(JSON.stringify(person));
